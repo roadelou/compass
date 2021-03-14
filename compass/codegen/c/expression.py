ed104ab46cedf96f7daef68bdaaeacca0c9e0117
roadelou <tableau.roche@gmail.com>
2021-03-14 16:26:03 +0100

    Added complex expressions in C backend

--- compass/codegen/c/expression.py
+++ compass/codegen/c/expression.py
@@ -36,9 +36,53 @@ def codegen_expression(expression: ast.Expression) -> str:
     elif isinstance(expression, ast.SignalExpression):
     # Getting the value behind the signal pointer.
         return f"*{expression.signal}"
+    elif isinstance(expression, ast.Binary):
+        return codegen_binary(expression)
+    elif isinstance(expression, ast.Unary):
+        return codegen_unary(expression)
     else:
         raise ValueError(f"Unkown Expression {expression}")
 
+def codegen_binary(expression: ast.Binary) -> str:
+    """
+    Specialized variant of codegen_expression for binary operators.
+    """
+    # Recursively building the code for the left and right expressions.
+    left_code = codegen_expression(expression.left)
+    right_code = codegen_expression(expression.right)
+    # We downcast to the right operator.
+    if isinstance(expression, ast.AndOp):
+        return f"({left_code} && {right_code})"
+    elif isinstance(expression, ast.ModOp):
+        return f"({left_code} % {right_code})"
+    elif isinstance(expression, ast.DivOp):
+        return f"({left_code} / {right_code})"
+    elif isinstance(expression, ast.MinusOp):
+        return f"({left_code} - {right_code})"
+    elif isinstance(expression, ast.OrOp):
+        return f"({left_code} || {right_code})"
+    elif isinstance(expression, ast.PlusOp):
+        return f"({left_code} + {right_code})"
+    elif isinstance(expression, ast.TestEqOp):
+        return f"({left_code} == {right_code})"
+    elif isinstance(expression, ast.TimesOp):
+        return f"({left_code} * {right_code})"
+    else:
+        raise ValueError(f"Unknown binary operator {expression}")
+
+def codegen_unary(expression: ast.Unary) -> str:
+    """
+    Specialized variant of codegen_expression for unary operators.
+    """
+    # Recursively building the code for the inner expression.
+    inner_code = codegen_expression(expression.expression)
+    # We downcast to the right operator.
+    if isinstance(expression, ast.NotOp):
+        return f"(!{inner_code})"
+    elif isinstance(expression, ast.UminusOp):
+        return f"(-{inner_code})"
+    else:
+        raise ValueError(f"Unknown unary operator {expression}")
 
 ##################################### MAIN #####################################
 
