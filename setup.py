36783e06c679f18024fdc7c98a0d375e79d27370
roadelou <tableau.roche@gmail.com>
2021-03-20 19:50:02 +0100

    Version bump to fix bugs in PyPi package

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
 
     setup(
         name="roadelou-compass",
-        version="0.0.3",
+        version="0.0.4",
         author="roadelou",
         author_email="",
         packages=[
