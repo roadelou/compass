284231f91eb58e4bb846ac512a31d65157cc1fc1
roadelou <tableau.roche@gmail.com>
2021-03-21 14:44:27 +0100

    Added compass codegen for new extern keyword

--- compass/codegen/compass/tree.py
+++ compass/codegen/compass/tree.py
@@ -22,6 +22,9 @@ import compass.ast as ast  # Used for downcasts
 from compass.codegen.compass.module import (
     codegen_module,
 )  # Used for recursive codegen
+from compass.codegen.compass.declaration import (
+    codegen_declaration,
+)  # Used for recursive codegen
 
 ################################### CLASSES ####################################
 
@@ -34,8 +37,32 @@ def codegen_tree(tree: ast.Tree) -> str:
     """
     Outputs the Compass source code for the provided AST.
     """
-    # For now an AST only contains one module.
-    return codegen_module(tree.module)
+    source_code = ""
+
+    # We start by building the code for all the extern declarations.
+    for extern in tree.externs:
+        # We build the source for the arguments of the extern submodule.
+        code_arguments = ", ".join(
+            [
+                codegen_declaration(declaration)
+                for declaration in extern.declarations
+            ]
+        )
+        # We add the source code for the entire declaration of the extern
+        # submodule.
+        source_code += f"extern {extern.name}({code_arguments});\n"
+    # If there are no extern modules, we just add a comment.
+    if len(tree.externs) == 0:
+        source_code += "# No extern submodules."
+
+    # A blank line for the style.
+    source_code += "\n"
+
+    # We add the source code for the main module.
+    source_code += codegen_module(tree.module)
+
+    # We return the complete source code.
+    return source_code
 
 
 ##################################### MAIN #####################################
