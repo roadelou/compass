daff45c7299a83f4983025520acabd04fd83f202
roadelou <tableau.roche@gmail.com>
2021-03-30 19:21:13 +0200

    Version bump for compass-builder

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def main():
 
     setup(
         name="roadelou-compass",
-        version="0.0.5",
+        version="0.0.6",
         author="roadelou",
         author_email="",
         packages=[
